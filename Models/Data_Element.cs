dca3ac5ad0e6b33a0b22f71da1e3d841c4eb6335
rabi0adhikari@gmail.com <rabi0adhikari@gmail.com>
2020-03-19 21:46:17 -0500

    Add project files.

---
+++ Models/Data_Element.cs
@@ -0,0 +1,63 @@
+﻿namespace DataGridProject.ViewModels
+{
+    public class Data_Element
+	{
+		private string _id;
+
+		public string Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		private string _uniqueId;
+
+		public string UniqueId
+		{
+			get { return _uniqueId; }
+			set { _uniqueId = value; }
+		}
+
+		private string _worksetId;
+
+		public string WorksetId
+		{
+			get { return _worksetId; }
+			set { _worksetId = value; }
+		}
+
+		private string _elementType;
+
+		public string ElementType
+		{
+			get { return _elementType; }
+			set { _elementType = value; }
+		}
+
+		private bool _isSelected;
+
+		public bool IsSelected
+		{
+			get { return _isSelected; }
+			set { _isSelected = value; }
+		}
+
+		private string _category;
+
+		public string Category
+		{
+			get { return _category; }
+			set { _category = value; }
+		}
+
+
+	}
+}
---
+++ ViewModels/WorksetsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace DataGridProject.ViewModels
+{
+    public class WorksetsViewModel
+    {
+        private ObservableCollection<Data_Element> _objectsCollection;
+
+        public ObservableCollection<Data_Element> ObjectsCollection
+        {
+            get { return _objectsCollection; }
+            set { _objectsCollection = value; }
+        }
+
+        public IList<string> _worksets;
+
+        public IList<string> Worksets
+        {
+            get { return _worksets; }
+            set { _worksets = value; }
+        }
+
+        public WorksetsViewModel()
+        {
+            Worksets = new List<string> { "Workset1", "Shared Levels & Grids", "Interior", "Exterior", "Structural" };
+            //Generate the Data that we need for data grid.
+            ObjectsCollection = ReadCSVFile();
+        }
+
+        public static ObservableCollection<Data_Element> ReadCSVFile()
+        {
+            using (var reader = new StreamReader(@"C:\Users\radhikari\OneDrive - RO\Documents\GitHub\DataGridProject\DataDataGrid.txt"))
+            {
+                ObservableCollection<Data_Element> ObjectsCollection = new ObservableCollection<Data_Element>();
+
+                string headerLine = reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('\t');
+                    var tempData = new Data_Element
+                    {
+                        Id = values[0],
+                        Name = values[1],
+                        ElementType = values[2],
+                        Category = values[3],
+                        UniqueId = values[4],
+                        WorksetId = values[5],
+                        IsSelected = false,
+                    };
+
+                    ObjectsCollection.Add(tempData);
+                }
+                return ObjectsCollection;
+            }
+        }
+    }
+}
