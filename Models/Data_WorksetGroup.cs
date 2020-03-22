bfea6d5af88263165387049be312c16cdc10a74e
rabi0adhikari@gmail.com <rabi0adhikari@gmail.com>
2020-03-22 14:22:32 -0500

    CollectionViewSource isn't something I could apply because of my limited knowledge about it. Going back to grouping based on methods.

---
+++ Models/Data_WorksetGroup.cs
@@ -0,0 +1,47 @@
+﻿using DataGridProject.ViewModels;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace DataGridProject.Models
+{
+    public class Data_WorksetGroup : BindableBase
+    {
+        //Create properties for observable collection of all Workset and its elements
+        private ObservableCollection<Data_Element> _items;
+
+        public ObservableCollection<Data_Element> Items
+        {
+            get { return _items; }
+            set { SetProperty(ref _items, value); }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private bool _isSelected;
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set { SetProperty(ref _isSelected, value); }
+        }
+
+        private bool _isExpanded;
+
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set { SetProperty(ref _isExpanded, value); }
+        }
+
+        public Data_WorksetGroup(ObservableCollection<Data_Element> items)
+        {
+            Items = items;
+        }
+    }
+}
--- ViewModels/WorksetsViewModel.cs
+++ ViewModels/WorksetsViewModel.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using DataGridProject.Models;
+using Prism.Mvvm;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 
 namespace DataGridProject.ViewModels
 {
-    public class WorksetsViewModel
+    public class WorksetsViewModel : BindableBase
     {
-        private ObservableCollection<Data_Element> _objectsCollection;
+        private ObservableCollection<Data_WorksetGroup> _objectsCollection;
 
-        public ObservableCollection<Data_Element> ObjectsCollection
+        public ObservableCollection<Data_WorksetGroup> ObjectsCollection
         {
             get { return _objectsCollection; }
-            set { _objectsCollection = value; }
+            set { SetProperty(ref _objectsCollection, value); }
         }
 
         public IList<string> _worksets;
@@ -19,21 +22,41 @@ namespace DataGridProject.ViewModels
         public IList<string> Worksets
         {
             get { return _worksets; }
-            set { _worksets = value; }
+            set { SetProperty(ref _worksets, value); }
         }
 
         public WorksetsViewModel()
         {
-            Worksets = new List<string> { "Workset1", "Shared Levels & Grids", "Interior", "Exterior", "Structural" };
+            Worksets = new ObservableCollection<string> { "Workset1", "Shared Levels & Grids", "Interior", "Exterior", "Structural" };
             //Generate the Data that we need for data grid.
-            ObjectsCollection = ReadCSVFile();
+            //ObjectsCollection = ReadCSVFile();
+            ObjectsCollection = new ObservableCollection<Data_WorksetGroup>();
+
+            //var queryWorksetId =
+            //        from item in ReadCSVFile()
+            //        group item by item.WorksetId into newGroup
+            //        select newGroup as ObservableCollection<Data_Element>;
+
+            var queryWorksetId = ReadCSVFile().GroupBy(x => x.WorksetId).Select(x => x).ToList();
+
+
+            foreach (var group in queryWorksetId)
+            {
+                Data_WorksetGroup data_WorksetGroup = new Data_WorksetGroup(new ObservableCollection<Data_Element>(group))
+                {
+                    Name = group.Key,
+                    IsSelected = true
+                };
+                ObjectsCollection.Add(data_WorksetGroup);
+            }
+
         }
 
         public static ObservableCollection<Data_Element> ReadCSVFile()
         {
             using (var reader = new StreamReader(@"C:\Users\radhikari\OneDrive - RO\Documents\GitHub\DataGridProject\DataDataGrid.txt"))
             {
-                ObservableCollection<Data_Element> ObjectsCollection = new ObservableCollection<Data_Element>();
+                ObservableCollection<Data_Element> items = new ObservableCollection<Data_Element>();
 
                 string headerLine = reader.ReadLine();
 
@@ -52,9 +75,9 @@ namespace DataGridProject.ViewModels
                         IsSelected = false,
                     };
 
-                    ObjectsCollection.Add(tempData);
+                    items.Add(tempData);
                 }
-                return ObjectsCollection;
+                return items;
             }
         }
     }
